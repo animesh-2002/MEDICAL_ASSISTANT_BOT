fcfa185591e67818ea5ca46c0132ff0585b6ed65
animesh-2002 <93852453+animesh-2002@users.noreply.github.com>
2025-06-16 12:26:45 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical Assitant Generative AI project",
+    version='0.0.0',
+    author='ANIMESH KAR',
+    author_email='animeshkumarkar24@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
